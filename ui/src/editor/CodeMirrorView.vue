ef19c0ba39ab88e2c59c3dd692e61a9cc5788e69
LIlGG <1103069291@qq.com>
2026-03-20 17:58:02 +0800

    pref: no update triggered when content is not changed

--- ui/src/editor/CodeMirrorView.vue
+++ ui/src/editor/CodeMirrorView.vue
@@ -33,7 +33,7 @@ import {
   nodeViewProps,
 } from "@halo-dev/richtext-editor";
 import MingcuteRightSmallFill from "~icons/mingcute/right-small-fill";
-import type { EditorSelection, Line, SelectionRange } from "@codemirror/state";
+import type { Line, SelectionRange } from "@codemirror/state";
 
 const props = defineProps(nodeViewProps);
 
@@ -124,25 +124,7 @@ const setupSplitView = () => {
 
   destroyCodeMirror();
 
-  cm = new CodeMirror({
-    doc: currentText,
-    extensions: [
-      autocompletion(),
-      cmKeymap.of([...codeMirrorKeymap(), ...defaultKeymap]),
-      drawSelection(),
-      syntaxHighlighting(defaultHighlightStyle),
-      CodeMirror.updateListener.of((update) => {
-        forwardUpdate(update);
-        if (update.docChanged) {
-          updateSplitPreview();
-        }
-      }),
-      ...(props.extension.options.extensions || []),
-    ],
-  });
-
-  editorContainerRef.value.innerHTML = "";
-  editorContainerRef.value.appendChild(cm.dom);
+  createCodeMirror(currentText);
 
   previewRenderer = new PreviewRenderer(
     blockType.value,
@@ -150,7 +132,9 @@ const setupSplitView = () => {
   );
   updateSplitPreview();
 
-  cm.focus();
+  if (cm) {
+    cm.focus();
+  }
 };
 
 const updateSplitPreview = () => {
@@ -173,11 +157,13 @@ const updateStandalonePreview = () => {
   previewRenderer.render(currentText);
 };
 
-const createCodeMirror = () => {
-  if (!editorContainerRef.value) return;
+const createCodeMirror = (doc: string): void => {
+  if (!editorContainerRef.value) {
+    return;
+  }
 
   cm = new CodeMirror({
-    doc: props.node.textContent,
+    doc,
     extensions: [
       autocompletion(),
       cmKeymap.of([...codeMirrorKeymap(), ...defaultKeymap]),
@@ -280,24 +266,23 @@ const forwardUpdate = (update: ViewUpdate) => {
   if (updating || !cm.hasFocus) {
     return;
   }
+  if (!update.docChanged) {
+    return;
+  }
+
   const pos = props.getPos?.();
   if (pos === undefined) {
     return;
   }
 
-  const nodeStart = pos;
-  const nodeEnd = pos + props.node.nodeSize;
   const pmSel = props.editor.view.state.selection;
-
-  if (pmSel.from < nodeStart || pmSel.from > nodeEnd) {
-    return;
-  }
 
   let offset = pos + 1;
   const { main } = update.state.selection;
   const selFrom = offset + main.from;
   const selTo = offset + main.to;
-  if (update.docChanged || pmSel.from != selFrom || pmSel.to != selTo) {
+
+  if (pmSel.from != selFrom || pmSel.to != selTo) {
     const tr = props.editor.view.state.tr;
     update.changes.iterChanges((fromA, toA, fromB, toB, text) => {
       if (text.length) {
@@ -318,7 +303,7 @@ const forwardUpdate = (update: ViewUpdate) => {
 
 const selectNode = () => {
   if (!cm) {
-    createCodeMirror();
+    createCodeMirror(props.node.textContent);
   }
   props.editor.chain().scrollIntoView().run();
   if (cm) {
